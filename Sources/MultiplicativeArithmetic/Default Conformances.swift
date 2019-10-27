c7ede3859d49d9d9ccb5c394bafd006c6bc2fe42
Ben Leggiero <BenLeggiero@Gmail.com>
2019-10-26 18:58:01 -0600

    #1: Made many stdlib types conform to `MutliplicativeArithmetic`

---
+++ Sources/MultiplicativeArithmetic/Default Conformances.swift
@@ -0,0 +1,32 @@
+//
+//  Default Conformances.swift
+//  MultiplicativeArithmetic
+//
+//  Created by Ben Leggiero on 2019-10-26.
+//  Copyright © 2019 Ben Leggiero BH-1-PS
+//
+
+import Foundation
+
+
+
+extension CGFloat: MultiplicativeArithmetic {}
+extension Float32: MultiplicativeArithmetic {}
+extension Float64: MultiplicativeArithmetic {}
+extension Float80: MultiplicativeArithmetic {}
+
+
+
+extension Int: MultiplicativeArithmetic {}
+extension Int8: MultiplicativeArithmetic {}
+extension Int16: MultiplicativeArithmetic {}
+extension Int32: MultiplicativeArithmetic {}
+extension Int64: MultiplicativeArithmetic {}
+
+
+
+extension UInt: MultiplicativeArithmetic {}
+extension UInt8: MultiplicativeArithmetic {}
+extension UInt16: MultiplicativeArithmetic {}
+extension UInt32: MultiplicativeArithmetic {}
+extension UInt64: MultiplicativeArithmetic {}
